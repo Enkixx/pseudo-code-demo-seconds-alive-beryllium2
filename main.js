2a475c3010656d814eccd8d5aacea05d6b2e8172
Enkixx <sean.barnell@gmail.com>
2021-06-24 15:08:44 -0400

    Setup javascript for seconds calculation

---
+++ main.js
@@ -0,0 +1,26 @@
+let birthYear = parseFloat(prompt("What year were you born?"));
+let birthMonth = parseFloat(prompt("What number month were you born?"));
+let birthDay = parseFloat(prompt("What day were you born?"));
+
+let currentDate = new Date();
+
+let birthDate = new Date(
+    birthYear,
+    birthMonth,
+    birthDay
+);
+
+let seconds = (currentDate.valueOf() - birthDate.valueOf()) / 1000;
+let secondsClean = Math.floor(seconds)
+
+if (isNaN(secondsClean)) {
+    alert("Please refresh and enter numbers for the date info requested");
+    document.write("<h2>Please Refresh</h2>")
+} else {
+    
+    document.write(
+        `<h2>Your life in seconds is `
+        + secondsClean
+        + ` seconds...<br><em>...and may you have many more!</em></h2>`
+    )
+}
